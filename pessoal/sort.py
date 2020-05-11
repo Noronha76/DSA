a27b72d487fed9f4a3195bc6e6227233c4cd017b
Noronha <felipetnoronha@icloud.com>
2020-05-11 18:35:37 -0300

    x1

--- pessoal/sort.py
+++ pessoal/sort.py
@@ -1,9 +1,12 @@
 import random
-lst = []
-[lst.append(random.randint(1,10)) for i in range(10)]
-print('lista original : ',lst)
+lsta, lstb = [], []
+[lsta.append(random.randint(1,10)) for i in range(10)]
+[lstb.append(random.randint(1,10)) for i in range(10)]
+print('listas originais : ',lsta,lstb)
+lst2 = lsta.sort()
+print('lista A ordenada : ',lsta) #doidera
 for i in range(10):
     for ii in range(i+1,10):
-        if(lst[i] > lst[ii]):
-            lst[i], lst[ii] = lst[ii], lst[i]
-print('lista ordenada : ',lst)
+        if(lstb[i] > lstb[ii]):
+            lstb[i], lstb[ii] = lstb[ii], lstb[i]
+print('lista B ordenada : ',lstb)
